dabf5fabb5f0d3b996b7c57cd62ff1a81b3812de
Garrett Albright <contact@albright.pro>
2019-09-16 17:06:12 -0600

    Redo how we do scaling to achieve a better aspect ratio.

--- dotart.js
+++ dotart.js
@@ -4,69 +4,75 @@ let DotArt = {};
 DotArt.maxCharCount = 1992;
 DotArt.verticalGap = 1;
 DotArt.horizontalGap = 1;
+DotArt.charAspectRatio = .5;
 // https://en.wikipedia.org/wiki/Relative_luminance
 DotArt.rScale = .2126;
 DotArt.gScale = .7152;
 DotArt.bScale = .0722;
 DotArt.dotToHex = [0x1, 0x8, 0x2, 0x10, 0x4, 0x20, 0x40, 0x80];
-DotArt.scaledImageData;
 
 DotArt.init = function() {
   document.getElementById('file-input').addEventListener('change', function(e) {
     e.preventDefault();
-    this.scaledImageData = null;
     let uploadedImage = e.target.files[0];
     let reader = new FileReader();
     reader.addEventListener('load', function(e) {
       let image = new Image();
       image.addEventListener('load', function(e) {
-        let charWidth = Math.ceil(image.width / (2 + this.verticalGap));
-        let charHeight = Math.ceil(image.height / (8 + this.horizontalGap));
-        let targetWidth = image.width;
-        let targetHeight = image.height;
-        let isTaller = image.height > image.width;
-        let aspectRatio = targetWidth / targetHeight;
-        // @todo factor line breaks when calculating character count
-        while (charWidth * charHeight > this.maxCharCount) {
-          if (isTaller) {
-            targetWidth--;
-            targetHeight = Math.round(targetWidth * (1 / aspectRatio));
-          }
-          else {
-            targetHeight--;
-            targetWidth = Math.round(targetHeight * aspectRatio);
-          }
-          charWidth = Math.ceil(targetWidth / (2 + verticalGap));
-          charHeight = Math.ceil(targetHeight / (8 + horizontalGap));
-        }
+        // Determine the smallest size of rectangles with the aspect ratio of
+        // the braille charaters which will give us a number of rectangles
+        // smaller than the limit
+        let cellHeight = 0;
+        let cellWidth;
+        let cellRows;
+        let cellColumns;
+        do {
+          cellHeight++;
+          cellWidth = cellHeight * DotArt.charAspectRatio;
+          cellRows = Math.ceil(image.height / cellHeight);
+          cellColumns = Math.ceil(image.width / cellWidth);
+        } while ((cellRows + 1) * cellColumns > DotArt.maxCharCount);
+        
         // Scale image via canvas
         let canvas = document.createElement('canvas');
+        let targetHeight = cellRows * 8;
+        let targetWidth = cellColumns * 2;
         canvas.setAttribute('width', targetWidth);
         canvas.setAttribute('height', targetHeight);
         let canvasContext = canvas.getContext('2d');
         canvasContext.drawImage(image, 0, 0, targetWidth, targetHeight);
         
+        // Do b&w conversion
+        let imageData = canvasContext.getImageData(0, 0, targetWidth, targetHeight);
+        let dataLen = imageData.data.length;
+        for (var pixelPos = 0; pixelPos < dataLen; pixelPos += 4) {
+          // Treat non-opaque pixels as black
+          let grayVal;
+          if (imageData.data[pixelPos + 3] < 255) {
+            grayVal = 0;
+          }
+          else {
+            grayVal = (imageData.data[pixelPos] * DotArt.rScale) + (imageData.data[pixelPos + 1] + DotArt.gScale) + (imageData.data[pixelPos + 2] * DotArt.bScale);
+          }
+          imageData.data[pixelPos] = imageData.data[pixelPos + 1] = imageData.data[pixelPos + 2] = (grayVal > 127 ? 255 : 0);
+        }
+        canvasContext.putImageData(imageData, 0, 0);
         let string = "";
-        for (var charY = 0; charY < charHeight; charY++) {
-          let yPixel = charY * (8 + this.horizontalGap);
-          for (var charX = 0; charX < charWidth; charX++) {
-            let xPixel = charX * (2 + this.verticalGap);
-            // https://en.wikipedia.org/wiki/Relative_luminance
-            let colors = canvasContext.getImageData(xPixel, yPixel, 2, 8).data;
-            let char = 0x2800;
+        for (var charY = 0; charY < cellRows; charY++) {
+          let yPixel = charY * 8;
+          for (var charX = 0; charX < cellColumns; charX++) {
+            let xPixel = charX * 2;
+            let cell = canvasContext.getImageData(xPixel, yPixel, 2, 8).data;
+            let character = 0x2800;
             for (var pip = 0; pip < 8; pip++) {
-              let offset = pip * 4;
-              // Convert to grayscale, then b&w
-              let grayVal = (colors[offset] * this.rScale) + (colors[offset + 1] * this.gScale) + (colors[offset] * this.bScale);
-              if (grayVal > 127) {
-                char += dotToHex[pip];
+              if (cell[pip * 4] == 255) {
+                character += DotArt.dotToHex[pip];
               }
             }
-            string += String.fromCharCode(char);
+            string += String.fromCharCode(character);
           }
           string += "\n";
         }
-//         console.log(string);
         let textNode = document.createTextNode(string);
         let resultNode = document.getElementById('result');
         while (resultNode.firstChild) {
