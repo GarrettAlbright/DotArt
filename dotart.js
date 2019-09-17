cdb31b445292027bc9303e7f08c74e488c858c2e
Garrett Albright <contact@albright.pro>
2019-09-16 19:25:56 -0600

    Move work after compiling the "graymap" into a separate funciton so we don't have to recalculate from the color image every time.

--- dotart.js
+++ dotart.js
@@ -10,6 +10,12 @@ DotArt.rScale = .2126;
 DotArt.gScale = .7152;
 DotArt.bScale = .0722;
 DotArt.dotToHex = [0x1, 0x8, 0x2, 0x10, 0x4, 0x20, 0x40, 0x80];
+DotArt.currentGraymap;
+DotArt.bwThreshold = 127;
+DotArt.cellRows;
+DotArt.cellColumns;
+DotArt.targetWidth;
+DotArt.targetHeight;
 
 DotArt.init = function() {
   document.getElementById('file-input').addEventListener('change', function(e) {
@@ -24,67 +30,85 @@ DotArt.init = function() {
         // smaller than the limit
         let cellHeight = 0;
         let cellWidth;
-        let cellRows;
-        let cellColumns;
         do {
           cellHeight++;
           cellWidth = cellHeight * DotArt.charAspectRatio;
-          cellRows = Math.ceil(image.height / cellHeight);
-          cellColumns = Math.ceil(image.width / cellWidth);
-        } while ((cellRows + 1) * cellColumns > DotArt.maxCharCount);
+          DotArt.cellRows = Math.ceil(image.height / cellHeight);
+          DotArt.cellColumns = Math.ceil(image.width / cellWidth);
+        } while ((DotArt.cellColumns + 1) * DotArt.cellRows > DotArt.maxCharCount);
         
         // Scale image via canvas
         let canvas = document.createElement('canvas');
-        let targetHeight = (cellRows * 8) + ((cellRows - 1) * DotArt.horizontalGap);
-        let targetWidth = (cellColumns * 2) + ((cellColumns - 1) * DotArt.verticalGap);
-        canvas.setAttribute('width', targetWidth);
-        canvas.setAttribute('height', targetHeight);
+        DotArt.targetHeight = (DotArt.cellRows * 8) + ((DotArt.cellRows - 1) * DotArt.horizontalGap);
+        DotArt.targetWidth = (DotArt.cellColumns * 2) + ((DotArt.cellColumns - 1) * DotArt.verticalGap);
+        canvas.setAttribute('width', DotArt.targetWidth);
+        canvas.setAttribute('height', DotArt.targetHeight);
         let canvasContext = canvas.getContext('2d');
-        canvasContext.drawImage(image, 0, 0, targetWidth, targetHeight);
+        canvasContext.drawImage(image, 0, 0, DotArt.targetWidth, DotArt.targetHeight);
         
-        // Do b&w conversion
-        let imageData = canvasContext.getImageData(0, 0, targetWidth, targetHeight);
+        // Build the graymap
+        DotArt.currentGraymap = [];
+        let imageData = canvasContext.getImageData(0, 0, DotArt.targetWidth, DotArt.targetHeight);
         let dataLen = imageData.data.length;
-        for (var pixelPos = 0; pixelPos < dataLen; pixelPos += 4) {
+        for (var pixelOffset = 0; pixelOffset < dataLen; pixelOffset += 4) {
           // Treat non-opaque pixels as black
           let grayVal;
-          if (imageData.data[pixelPos + 3] < 255) {
+          if (imageData.data[pixelOffset + 3] < 255) {
             grayVal = 0;
           }
           else {
-            grayVal = (imageData.data[pixelPos] * DotArt.rScale) + (imageData.data[pixelPos + 1] + DotArt.gScale) + (imageData.data[pixelPos + 2] * DotArt.bScale);
+            grayVal = (imageData.data[pixelOffset] * DotArt.rScale) + (imageData.data[pixelOffset + 1] + DotArt.gScale) + (imageData.data[pixelOffset + 2] * DotArt.bScale);
           }
-          imageData.data[pixelPos] = imageData.data[pixelPos + 1] = imageData.data[pixelPos + 2] = (grayVal > 127 ? 255 : 0);
+          DotArt.currentGraymap.push(grayVal);
         }
-        canvasContext.putImageData(imageData, 0, 0);
-        let string = "";
-        for (var charY = 0; charY < cellRows; charY++) {
-          let yPixel = charY * (8 + DotArt.horizontalGap);
-          for (var charX = 0; charX < cellColumns; charX++) {
-            let xPixel = charX * (2 + DotArt.verticalGap);
-            let cell = canvasContext.getImageData(xPixel, yPixel, 2, 8).data;
-            let character = 0x2800;
-            for (var pip = 0; pip < 8; pip++) {
-              if (cell[pip * 4] == 255) {
-                character += DotArt.dotToHex[pip];
-              }
-            }
-            string += String.fromCharCode(character);
+/*
+        let previewCanvas = document.getElementById('canvas');
+        previewCanvas.setAttribute('width', DotArt.targetWidth);
+        previewCanvas.setAttribute('height', DotArt.targetHeight);
+        let previewContext = previewCanvas.getContext('2d');
+        for (var px = 0; px < DotArt.currentGraymap.length; px++) {
+          let y = Math.floor(px / DotArt.targetWidth);
+          let x = px % DotArt.targetWidth;
+          let currentGray = DotArt.currentGraymap[px];
+          previewContext.fillStyle = 'rgb(' + currentGray + ',' + currentGray + ',' + currentGray + ')';
+          previewContext.fillRect(x, y, 1, 1);
+          if (y > 0) {
+            continue;
           }
-          string += "\n";
         }
-        let textNode = document.createTextNode(string);
-        let resultNode = document.getElementById('result');
-        while (resultNode.firstChild) {
-          resultNode.removeChild(resultNode.firstChild);
-        }
-        resultNode.appendChild(textNode);
+*/
+        DotArt.convertFromGraymap();        
       });
       image.src = e.target.result;
     });
     reader.readAsDataURL(uploadedImage);
   });
 };
+
+DotArt.convertFromGraymap = function() {
+    let string = "";
+    let rowWidth = DotArt.cellColumns * 2;
+    for (var charY = 0; charY < DotArt.cellRows; charY++) {
+      for (var charX = 0; charX < DotArt.cellColumns; charX++) {
+        let character = 0x2800;
+        let graymapOffset = ((charY * DotArt.targetWidth * (8 + DotArt.horizontalGap))) + (charX * (2 + DotArt.verticalGap));
+        for (var pip = 0; pip < 8; pip++) {
+          let graymapPos = graymapOffset + (Math.floor(pip / 2) * DotArt.targetWidth) + (pip % 2);
+          if (DotArt.currentGraymap[graymapPos] > DotArt.bwThreshold) {
+            character += DotArt.dotToHex[pip];
+          }
+        }
+        string += String.fromCharCode(character);
+      }
+      string += "\n";
+    }
+    let textNode = document.createTextNode(string);
+    let resultNode = document.getElementById('result');
+    while (resultNode.firstChild) {
+      resultNode.removeChild(resultNode.firstChild);
+    }
+    resultNode.appendChild(textNode);
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   DotArt.init();
